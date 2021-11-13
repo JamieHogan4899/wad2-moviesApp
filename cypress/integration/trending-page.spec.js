d7eecd5948470ebb200d09e9c1a9557172c94809
Jamie Hogan <20084469@mail.wit.ie>
2021-11-13 14:52:19 +0000

    check page test

---
+++ cypress/integration/trending-page.spec.js
@@ -0,0 +1,36 @@
+let movies;    // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
+describe("Home Page ", () => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")    // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results
+      })
+  })
+  beforeEach(() => {
+    cy.visit("/movies/Trending")
+  });
+
+  describe("Base tests", () => {
+    describe("Base test", () => {
+        it("displays page header", () => {
+          cy.get("h3").contains("Trending Movies");
+          cy.get("h1").contains("Filter the movies");
+        });
+      });
+  });
+
+});
