d5e3a1f2a1f041a4e7e4857d5a963ae556e38ec2
Jamie Hogan <20084469@mail.wit.ie>
2021-11-30 17:08:01 +0000

    tv shows basic setup

--- src/index.js
+++ src/index.js
@@ -37,8 +37,9 @@ const App = () => {
         <Route exact path="/reviews/form" component={AddMovieReviewPage} />
         <Route exact path="/movies/upcoming" component={UpcomingMoviePage} />  
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-        <Route exact path="/movies/tvShowsPage" component={TvShowsPage} />
+        <Route exact path="/shows/tvShowsPage" component={TvShowsPage} />
         <Route exact path="/movies/:id" component={MoviePage} /> 
+        <Route exact path="/shows/:id" component={MoviePage} /> 
         <Route exact path="/" component={HomePage} />
         <Route path="/reviews/:id" component={MovieReviewPage} />
         <Redirect from="*" to="/" />
--- src/pages/tvShowsPage.js
+++ src/pages/tvShowsPage.js
@@ -1,13 +1,13 @@
 import React from "react";
-import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
-import {getMovies} from '../api/tmdb-api'
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import {getTvShows} from '../api/tmdb-api'
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
+import PageTemplate from "../components/templateTvShowList";
 
 
 const TvShowsPage = (props) => {
-  const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
+  const {  data, error, isLoading, isError }  = useQuery('tvShows', getTvShows)
 
   if (isLoading) {
     return <Spinner />
@@ -16,20 +16,18 @@ const TvShowsPage = (props) => {
   if (isError) {
     return <h1>{error.message}</h1>
   }  
-  const movies = data.results;
+  const shows = data.results;
 
   // Redundant, but necessary to avoid app crashing.
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+
 
 
   return (
     <PageTemplate
       title="TV Shows"
-      movies={movies}
-      action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
-      }}
+      shows={shows}
+    
+      
     />
 );
 };
