e22dca0f3a801a12337f605fcaeed2d5af6784d5
Jamie Hogan <20084469@mail.wit.ie>
2021-12-01 14:16:28 +0000

    navagation between home page and trending page completed

--- cypress/integration/trending-page.spec.js
+++ cypress/integration/trending-page.spec.js
@@ -38,21 +38,30 @@ describe("Home Page ", () => {
 
   describe("Navagation Tests", () => {     
     it("Navagation Between Home Page and Trending Page", () => {
-      cy.get("header").find(".MuiToolbar-root").find("button").eq(3).click();
-      cy.get("h3").contains("Trending Movies");
-      cy.get("header").find(".MuiToolbar-root").find("button").eq(0).click();
-      cy.get("h3").contains("Discover Movies");
-      cy.get("button[aria-label='go back'").click();
-      cy.get("h3").contains("Trending Movies");;
-      cy.get("button[aria-label='go forward'").click();
-      cy.get("h3").contains("Discover Movies");;
-
-     
-     
-
-       
-  
+      cy.get("header").find(".MuiToolbar-root").find("button").eq(3).click();  //use header to go to trending
+      cy.get("h3").contains("Trending Movies");  //check page is trending
+      cy.get("header").find(".MuiToolbar-root").find("button").eq(0).click();  //go back home using header
+      cy.get("h3").contains("Discover Movies"); //check my page is home
+      cy.get("button[aria-label='go back'").click();  //use back button to go back to trending 
+      cy.get("h3").contains("Trending Movies"); //check page is trending 
+      cy.get("button[aria-label='go forward'").click();  //use forword button to go back home
+      cy.get("h3").contains("Discover Movies");  //check im back home
     });
 
-  });
+
+    it("Navagation between more info to favourties using trending page", () => {
+      cy.get("header").find(".MuiToolbar-root").find("button").eq(3).click();
+      cy.get("h3").contains("Trending Movies");
+
+      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+      cy.url().should("include", `/movies/${movies[0].id}`);
+      cy.get("h3").contains(movies[0].title);
+
+
+
+});
+
+
+
+});
 
