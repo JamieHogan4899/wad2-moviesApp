cb5074ff2c628953c27b7ad23016f44c1e2076c4
Jamie Hogan <20084469@mail.wit.ie>
2021-12-02 17:04:09 +0000

    Tv shows page header updated to include title + back and forword buttons

--- src/components/templateTvShowList/index.js
+++ src/components/templateTvShowList/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import ShowList from "../showList";
-//import Header from "../headerMovieList";
+import Header from "../headerMovieList";
 
 
 
-function tvShowPageTemplate({ shows, action //title 
+function tvShowPageTemplate({ shows, action, title 
  }) {
   
 
   return (
     <Grid container>
       <Grid item xs={12}>
-      {/* <Header title={title} /> */}
+      {<Header title={title} /> }
       </Grid>
       <Grid item container spacing={5}>
         
-        <Grid key="find">
+        <Grid key="find"  >
         
           
         </Grid>
