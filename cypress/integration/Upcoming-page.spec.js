77d7f92f3b3f97234399c74d9f693b0ea7445cbb
Jamie Hogan <20084469@mail.wit.ie>
2021-12-03 21:56:38 +0000

    tested more info button on trending page

--- cypress/integration/Upcoming-page.spec.js
+++ cypress/integration/Upcoming-page.spec.js
@@ -38,38 +38,42 @@ const filterByGenre = (movieList, genreId) =>
     describe("Navagation Tests", () => {     
 
         before(() => {
-          cy.request(
-            `https://api.themoviedb.org/3/trending/movie/day?api_key=${Cypress.env(
-              "TMDB_KEY"
-            )}&language=en-US&include_adult=false&include_video=false&page=1`
-    
-          )
+            cy.request(
+              `https://api.themoviedb.org/3/movie/upcoming?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=1`
+      
+            )
             .its("body")
             .then((response) => {
               Upcoming = response.results;
             });
           });
-    
-            
-      beforeEach(() => {
-        cy.visit("/movies/Upcoming")
-      });
-    
         
-    
-
+          beforeEach(() => {
+            cy.visit("/movies/Upcoming")
+          });
+        
           it("Navagation Between Home Page and Upcoming Page", () => {
-            
-            cy.get("header").find(".MuiToolbar-root").find("button").eq(0).click();  //go back home using header
+            cy.get("header").find(".MuiToolbar-root").find("button").eq(2).click();  //go back home using header
             cy.get("h3").contains("Discover Movies"); //check my page is home
-      
+
             cy.get("button[aria-label='go back'").click();  //use back button to go back to trending 
             cy.get("h3").contains("Upcoming Movies");; //check page is trending 
       
             cy.get("button[aria-label='go forward'").click();  //use forword button to go back home
             cy.get("h3").contains("Discover Movies");;  //check im back home
-          });
-      
-        
 
-        });
+    });
+
+    it("Testing to check the more info button on upcoming page", () => {
+
+     cy.wait(100)   //wait to go back to Upcoming page
+        
+      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click(); //Click on card 1 more info button 
+      cy.url().should("include", `/movies/${Upcoming[0].id}`); //check url is new one 
+   
+      
+      cy.get("header").find(".MuiToolbar-root").find("button").eq(4).click();  //use header to go to Upcoming
+      cy.get("h3").contains("Upcoming Movies");  //check page is favourites 
+
+});
+});
