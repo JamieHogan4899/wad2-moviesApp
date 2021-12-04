1b29b0652f00ed0b3875ee68b5ae33c66804e1a3
Jamie Hogan <20084469@mail.wit.ie>
2021-12-04 01:51:28 +0000

    test to check if the more info button works and navigate back to tvShow page

--- cypress/integration/tv-show-page.spec.js
+++ cypress/integration/tv-show-page.spec.js
@@ -1,24 +1,21 @@
-let movies;    // List of movies from TMDB
 let tvShows;   
 
 // Utility functions
-const filterByTitle = (movieList, string) =>
-  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+const filterByTitle = (showList, string) =>
+  showList.filter((m) => s.name.toLowerCase().search(string) !== -1);
 
-const filterByGenre = (movieList, genreId) =>
-  movieList.filter((m) => m.genre_ids.includes(genreId));
+const filterByGenre = (showList, genreId) =>
+  showList.filter((m) => s.genre_ids.includes(genreId));
 
   describe("Tv Show Page ", () => {
     before(() => {
       // Get movies from TMDB and store in movies variable.
       cy.request(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
-            "TMDB_KEY"
-          )}&language=en-US&include_adult=false&include_video=false&page=1`
+        `https://api.themoviedb.org/3/tv/popular?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=1`
         )
         .its("body")    // Take the body of HTTP response from TMDB
         .then((response) => {
-          movies = response.results
+          tvShows = response.results
         })
     })
 
@@ -66,8 +63,22 @@ const filterByGenre = (movieList, genreId) =>
             cy.get("button[aria-label='go forward'").click();  //use forword button to go back home
             cy.get("h3").contains("Discover Movies");;  //check im back home
 });
+
+            it("Testing to check the more info button works and can navigate back to TvShow page", () => {
+
+            cy.wait(100)   //wait to go back to Tv show page    
+
+            cy.get(".MuiCardActions-root").eq(0).contains("More Info").click(); //Click on card 1 more info button 
+            cy.url().should("include", `/shows/${tvShows[0].id}`); //check url is new one 
+
+            
+            cy.get("header").find(".MuiToolbar-root").find("button").eq(6).click();  //use header to go to Upcoming
+            cy.get("h3").contains("TV Shows");  //check page is favourites 
+
 });
 });
+});
+
 
 
 
