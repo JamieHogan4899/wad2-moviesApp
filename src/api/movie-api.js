2e7c26dc074c76c987b99b338c8ed54949d04af6
Jamie Hogan <20084469@mail.wit.ie>
2021-12-29 17:47:24 +0000

    discover movies page working with new api

--- src/api/movie-api.js
+++ src/api/movie-api.js
@@ -20,11 +20,11 @@ export const signup = (username, password) => {
 
 export const getMovies = () => {
     return fetch(
-       '/api/movies',{headers: {
-         'Authorization': window.localStorage.getItem('token')
-      }
-    }
-    ).then(res => res.json());
-  };
+       'api/movies/tmdb/discover',{headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+        }
+        ).then(res => res.json());
+    };
 
  
