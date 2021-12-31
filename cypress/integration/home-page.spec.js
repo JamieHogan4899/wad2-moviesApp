0f4ee185f6ae2f4f8cec01bbb79343c0ae2a16c5
Jamie Hogan <20084469@mail.wit.ie>
2021-12-31 15:39:42 +0000

    fixed title test bug

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -51,7 +51,7 @@ describe("Home Page ", () => {
   describe("Filtering", () => {
     describe("By movie title", () => {
      it("should only display movies with m in the title", () => {
-       let searchString = "m";
+       let searchString = "spider";
        let matchingMovies = filterByTitle(movies, searchString);
        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
        cy.get(".MuiCardHeader-content").should(
